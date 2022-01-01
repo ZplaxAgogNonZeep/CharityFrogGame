282dea311dbeabd090d3bab7a3e99c2b5ad530ae
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-01-01 03:04:16 -0500

    Implemented Index Searching

--- Revenge of the Kerromancer/KerromancerPlayer/IndexSearch.gd
+++ Revenge of the Kerromancer/KerromancerPlayer/IndexSearch.gd
@@ -1,13 +1,23 @@
 extends Node
 
+const TestGun1 = "res://KerromancerPlayer/Weapons/TestGun1/TestGun1.tscn"
+
 const TestMagic1 = "res://KerromancerPlayer/Magic/Spells/TestMagic1/TestMagic1.tscn"
 const TestMagic2 = "res://KerromancerPlayer/Magic/Spells/TestMagic2/TestMagic2.tscn"
 const TestMagic3 = "res://KerromancerPlayer/Magic/Spells/TestMagic3/TestMagic3.tscn"
 
-const testDict = {"TestMagic1" : TestMagic1}
-
 func searchWeaponIndex(name):
-	pass
+	match name:
+		"TestGun1":
+			return preload(TestGun1)
 
 func searchMagicIndex(name):
-	preload(TestMagic1)
+	match name:
+		"TestMagic1":
+			return preload(TestMagic1)
+		"TestMagic2":
+			return preload(TestMagic2)
+		"TestMagic3":
+			return preload(TestMagic3)
+		_:
+			return null
