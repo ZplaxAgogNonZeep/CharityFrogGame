3fec273db323d102198b0caf414db2218bc37e79
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-06-18 14:53:25 -0400

    Began work on Power Lines and Boxes

---
+++ Revenge of the Kerromancer/Scenes/Interactables/LevelMechanics/PowerBox.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+# ELECTRICITY INHERENT FUNCTIONS ================================================
+
+func power(outlet, liveStatus):
+	pass
--- Revenge of the Kerromancer/Scenes/Interactables/LevelMechanics/PowerLine.gd
+++ Revenge of the Kerromancer/Scenes/Interactables/LevelMechanics/PowerLine.gd
@@ -5,13 +5,31 @@ var active = false
 func _ready():
 	if $Connection.animation == "Corner":
 		$OutletB.position = $Position2D.position
+
+func power(outlet, liveStatus):
+	setActive(liveStatus)
+	var connection = null
 	
+	if outlet == "OutletA":
+		if $OutletB.get_overlapping_areas().size() > 0:
+			connection = $OutletB.get_overlapping_areas()[0]
+	elif outlet == "OutletB":
+		if $OutletA.get_overlapping_areas().size() > 0:
+			connection = $OutletA.get_overlapping_areas()[0]
+	
+	if connection != null:
+		connection.power(connection.name, liveStatus)
 
-func checkForConnection():
-	var OutletA : bool
-	if $OutletA.get_overlapping_areas() != []:
-		OutletA = true
+func setActive(status):
+	active = status
+	if active:
+		$Center.set_animation("Active")
+	else:
+		$Center.set_animation("Inactive")
 
-	var OutletB : bool
-	if $OutletB.get_overlapping_areas() != []:
-		OutletB = true
+
+# ELECTRICITY INHERENT FUNCTIONS ================================================
+
+#func power(outlet, liveStatus):
+#	pass
+
